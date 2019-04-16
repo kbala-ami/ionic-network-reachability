4ca99d899c76968340e02ad026a00793cdd7f6c5
joshuva george <joshuva@adaptivemedia.in>
2019-04-16 14:30:17 +0530

    added basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const Greeter = (name: string) => `Hello ${name}`; 
+export async function isReachable() {
+    try {
+      const result = await fetch('https://httpbin.org/');
+      if (result.ok) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
+  }
