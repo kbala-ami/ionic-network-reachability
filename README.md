a6c851d3083110bb21d96db07228c1d7b2595723
joshuva george <joshuva@adaptivemedia.in>
2019-04-17 14:37:46 +0530

    added functionality for network monitoring

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# reachability
-Detect the internet access.
+# Ionic Network Reachability
+
+Ionic Network Reachability, it is used for `testing` network connection.
+
+### Ionic Network Reachability
+
+```bash
+npm install ionic-network-reachability --save
+```
+
+# Usage
+
+app.component.ts
+
+```ts
+import { isReachable } from 'ionic-network-reachability';
+
+export class AppComponent {
+    async checkOnline() {
+        try {
+            const isOnline = isReachable('url is optional');
+            return isOnline;
+        } catch(err) {
+            
+        }
+    }
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,47 @@
-export async function isReachable() {
+import { interval } from 'rxjs';
+import { filter, switchMap } from 'rxjs/operators';
+export class IonicNetworkReachability {
+  public isOnline: boolean;
+  constructor() {
+    this.isOnline = navigator.onLine;
+  }
+  public async isReachable(url: string = 'https://httpbin.org/') {
     try {
-      const result = await fetch('https://httpbin.org/');
+      const result = await fetch(url);
       if (result.ok) {
         return true;
       } else {
         return false;
       }
     } catch (error) {
-      return false;
+      throw error;
     }
   }
+  /**
+   * @description it is used monitor network activity
+   * @returns return observable.
+   */
+  public monitorNetworkActivity() {
+    return interval(1000).pipe(
+      filter(x => {
+        if (navigator.onLine) {
+          return true;
+        } else {
+          return false;
+        }
+      }),
+      switchMap(async isOnline => {
+        try {
+          const networkResult = await fetch('https://httpbin.org/');
+          if (networkResult.ok) {
+            return true;
+          } else {
+            return false;
+          }
+        } catch (error) {
+          return false;
+        }
+      }),
+    );
+  }
+}
